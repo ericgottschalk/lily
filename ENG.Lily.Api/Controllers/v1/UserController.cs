0224505a7163a917355fb6a78d2d4dcac6c0071e
ericgottschalk <ericng04@hotmail.com>
2018-10-13 03:18:08 -0300

    Rename controller.

--- ENG.Lily.Api/Controllers/v1/UserController.cs
+++ ENG.Lily.Api/Controllers/v1/UserController.cs
@@ -1,27 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using ENG.Lily.Application.Contracts;
+﻿using ENG.Lily.Application.Contracts;
 using ENG.Lily.Application.Mapping.Model;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ENG.Lily.Api.Controllers.v1
 {
     [Produces("application/json")]
     [Route("api/developer")]
-    public class DeveloperController : Controller
+    public class UserController : Controller
     {
         private readonly IUserApplication userApplication;
 
-        public DeveloperController(IUserApplication userApplication)
+        public UserController(IUserApplication userApplication)
         {
             this.userApplication = userApplication;
         }
 
         [HttpPost]
-        public IActionResult Save([FromBody] UserModel model)
+        public IActionResult Register([FromBody] UserModel model)
         {
             this.userApplication.Save(model);
 
