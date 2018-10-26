b2ffcbd8dd0a8d3527a0d40faf417039e5fa5c76
ericgottschalk <ericng04@hotmail.com>
2018-10-25 23:59:33 -0300

    Add migration 8.0

--- ENG.Lily.Infrastructure.Repository/Migrations/20181026025909_8.0.Designer.cs
+++ ENG.Lily.Infrastructure.Repository/Migrations/20181026025909_8.0.Designer.cs
@@ -4,14 +4,16 @@ using ENG.Lily.Infaestructure.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace ENG.Lily.Infrastructure.Repository.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20181026025909_8.0")]
+    partial class _80
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ ENG.Lily.Infrastructure.Repository/Migrations/20181026025909_8.0.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ENG.Lily.Infrastructure.Repository.Migrations
+{
+    public partial class _80 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Verified",
+                table: "User",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Verified",
+                table: "User");
+        }
+    }
+}
